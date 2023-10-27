76703c80b067a893ab5922f32e5cc329941b1ea2
Michaela <michaela.landova1@gmail.com>
2023-10-27 09:25:02 +0200

    correction of indentation

--- main.py
+++ main.py
@@ -3,34 +3,32 @@ board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
 print("Let's play Tic Tac Toe!")
 
 for line in board:
-  for column in line:
-    print(column, end=" ")
-  print()
+    for column in line:
+        print(column, end=" ")
+    print()
 
 player = input("Choose your sign(X or O): ").upper()
 
 while True:
 
-  position = input(f"Player {player}, choose your position(line and column): ")
-  line = int(position[0])
-  column = int(position[1])
-
-  while board[line][column] != "_":
-
-    print("This position is already taken, choose another one")
-    position = input(
-        f"Player {player}, choose your position(line and column): ")
+    position = input(f"Player {player}, choose your position(line and column): ")
     line = int(position[0])
     column = int(position[1])
 
-  board[line][column] = player
+    while board[line][column] != "_":
+        print("This position is already taken, choose another one")
+        position = input(f"Player {player}, choose your position(line and column): ")
+        line = int(position[0])
+        column = int(position[1])
 
-  for line in board:
-    for column in line:
-      print(column, end=" ")
-    print()
+    board[line][column] = player
 
-  if player == "X":
-    player = "O"
-  else:
-    player = "X"
+    for line in board:
+        for column in line:
+            print(column, end=" ")
+        print()
+
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
