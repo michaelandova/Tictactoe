b305a2a5309fc5e56695e21cd1f820073d3ed441
Michaela <michaela.landova1@gmail.com>
2024-01-07 14:34:59 +0100

    Add game mode "PvC" (choose random position for computer move)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def initialize_board(num_rows, num_columns):
-    return [["_" for _ in range(num_rows)] for _ in range(num_columns)]
+from random import choice
+from time import sleep
+
+
+#
+def initialize_board():
+    return [["_" for _ in range(row_length)] for _ in range(column_length)]
 
 
 def is_position_out_of_range(row, column):
@@ -34,6 +39,19 @@ def switch_player(player):
     return "O" if player == "X" else "X"
 
 
+def choose_game_mode():
+    while True:
+        mode = input("Choose the game mode (1 for Player vs. Player, 2 for Player vs. Computer): ")
+        if mode == "1":
+            print("You have chosen Player vs. Player mode.")
+            return "PvP"
+        elif mode == "2":
+            print("You have chosen Player vs. Computer mode.")
+            return "PvC"
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+
 def make_move():
     while True:
         position = input(
@@ -54,6 +72,25 @@ def make_move():
             print(
                 "Invalid input, please enter two digits representing row and column."
             )
+
+
+def check_possible_positions():
+    possible_positions = []
+    for row in range(len(board)):
+        for column in range(len(board[0])):
+            if board[row][column] == "_":
+                possible_positions.append((row, column))
+    return possible_positions
+
+
+def computer_move():
+    if len(check_possible_positions()) > 0:
+        computer_position = choice(check_possible_positions())
+        row, column = int(computer_position[0]), int(computer_position[1])
+        board[row][column] = computer_player
+        return computer_player
+    else:
+        return None
 
 
 def check_next(row, column, d_row, d_column):
@@ -86,7 +123,7 @@ def check_draw():
     for row in range(len(board)):
         for column in range(len(board[0])):
             if board[row][column] == "_":
-                return False  # empty cell and no winner was found
+                return False  # empty cell was found
     return True
 
 
@@ -94,29 +131,62 @@ while True:
     # initialization of the board
     row_length = 3
     column_length = row_length
-    winning_streak_length = 4
-    board = initialize_board(row_length, column_length)
+    winning_streak_length = 3
+    board = initialize_board()
 
     # the game
     print("Let's play Tic Tac Toe!\n")
     print_board()
+    game_mode = choose_game_mode()
     current_player = get_player_sign()
+    computer_player = switch_player(current_player)
 
     while True:
-        make_move()
-        print_board()
+        if game_mode == "PvP":
+            make_move()
+            print_board()
+            # check winner after every move
+            winner = check_winner()
+            if winner:
+                print(f"Player {winner} wins!")
+                break
+            draw = check_draw()
+            if draw is not False:
+                print("It's a draw!")
+                break
+            current_player = switch_player(current_player)
 
-        # check winner after every move
-        winner = check_winner()
-        if winner:
-            print(f"Player {winner} wins!")
-            break
-        draw = check_draw()
-        if draw is not False:
-            print("It's a draw!")
-            break
-        current_player = switch_player(current_player)
+        elif game_mode == "PvC":
+            make_move()
+            print_board()
+
+            winner = check_winner()
+            if winner:
+                print(f"Player {winner} wins!")
+                break
+            draw = check_draw()
+            if draw is not False:
+                print("It's a draw!")
+                break
+
+            computer_move()
+            sleep(0.5)
+            print("Computer turn")
+            sleep(1)
+            print_board()
+
+            winner = check_winner()
+            if winner:
+                print(f"Player {winner} wins!")
+                break
+            draw = check_draw()
+            if draw is not False:
+                print("It's a draw!")
+                break
 
     play_again = input("Do you want to play again? (yes/no): ").lower()
-    if play_again != "yes":
+    if play_again == "yes":
+        continue
+    else:
+        print(":(")
         break
